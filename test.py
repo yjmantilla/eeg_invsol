e8a0409b7dd6d295b75a8d9c6834f0b5176cf874
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2022-07-23 19:12:47 -0300

    fsaverage

--- test.py
+++ test.py
@@ -4,11 +4,17 @@ import mne
 import numpy as np
 
 subjects_dir = 'data/freesurfer/subjects'
-subject = 'FS_P005'
-eeg_path = "data/subjects/P005/2022-2478_T1_P1_P005.vhdr"
+subject = 'P005'
 eeg_locs = False #"data/ASP-64.bvef"
+subject_path = f'data\subjects\{subject}'
+eeg_path = os.path.join(subject_path,"2022-2478_T1_P1_P005.vhdr")
+
 SPACING = 'oct5'
-PREFLOOD=25
+PREFLOOD=25 #https://surfer.nmr.mgh.harvard.edu/fswiki/FsTutorial/SkullStripFix_freeview
+"""
+the default preflooding height is 25, which produces the cleanest skull strip for most cases. There aren't any hard and fast rules about how to select your height value, but as a general rule of thumb, if part of the brain is missing, you should start with a watershed threshold around 35, and if too much skull is remaining, you should start with a threshold of around 15.
+"""
+
 #%% Save a fif file of eeg + location info
 raw = mne.io.read_raw_brainvision(eeg_path, preload=True,eog=('EOG1','HEOGL', 'HEOGR', 'VEOGb'))
 raw = raw.pick_types(eeg=True) # Remove non-eeg channels
@@ -18,7 +24,8 @@ if eeg_locs:
 
     raw = raw.set_montage(montage)
 
-raw.save(eeg_path.replace('.vhdr','_raw.fif'),tmin=0,tmax=10,overwrite=True) #10 seconds to make test fast, should be None in real case
+raw_fpath=eeg_path.replace('.vhdr','_raw.fif')
+raw.save(raw_fpath,tmin=0,tmax=10,overwrite=True) #10 seconds to make test fast, should be None in real case
 
 #%% Make source space
 src_path = op.join(op.dirname(eeg_path),f'{subject}-{SPACING}-src.fif')
@@ -31,29 +38,49 @@ else:
 
 bem_path = os.path.join(subjects_dir,subject,'bem')
 bem_files = [os.path.join(bem_path,x) for x in ['inner_skull.surf','outer_skin.surf','outer_skull.surf']]
+model_fname = op.join(bem_path, f'{subject}-5120-bem.fif')
+bem_fname = op.join(bem_path, f'{subject}-5120-bem-sol.fif')
 
 #%% Make watershed BEM surfaces
 if not np.all([os.path.exists(x) for x in bem_files]):
     mne.bem.make_watershed_bem(subject, subjects_dir,preflood=PREFLOOD, overwrite=False)
 
 #%% Revisar si las capas quedaron bien
-mne.viz.plot_bem(subject, subjects_dir=subjects_dir)
+#mne.viz.plot_bem(subject, subjects_dir=subjects_dir)
 
 #%% Do coregistration
-mne.gui.coregistration(subjects_dir=subjects_dir)
+#mne.gui.coregistration(subjects_dir=subjects_dir)
 
-#%% Make BEM
-mne.viz.plot_bem(subject, subjects_dir=subjects_dir)
-
+trans_fname = os.path.join(subject_path,f'{subject}_trans.fif')
 #https://mne.tools/stable/overview/faq.html#faq-watershed-bem-meshes
-# Create a BEM model for a subject
-surfaces = mne.make_bem_model(subject, ico=3,subjects_dir=subjects_dir)#,conductivity=[0.3])
 
-# Write BEM surfaces to a fiff file
-# mne.write_bem_surfaces(model_fname, surfaces)
+if not os.path.exists(model_fname):
+    # Create a BEM model for a subject
+    surfaces = mne.make_bem_model(subject, ico=3,subjects_dir=subjects_dir)#,conductivity=[0.3])
+    # Write BEM surfaces to a fiff file
+    mne.write_bem_surfaces(model_fname, surfaces)
+else:
+    surfaces = mne.read_bem_surfaces(model_fname)
 
 # Create a BEM solution using the linear collocation approach
-# bem = mne.make_bem_solution(surfaces)
-# mne.write_bem_solution(bem_fname, bem)
+if not os.path.exists(bem_fname):
+    bem = mne.make_bem_solution(surfaces)
+    mne.write_bem_solution(bem_fname, bem)
+else:
+    bem = mne.read_bem_solution(bem_fname)
 
-# print(('\n*** BEM solution file {} written ***\n'.format(bem_fname)))
+"""Compute leadfield matrix by BEM."""
+data_path, raw_fname = os.path.split(raw_fpath)
+raw_fname = os.path.splitext(raw_fname)[0]
+fwd_filename = raw_fname + '-' + SPACING
+
+if not os.path.exists(fwd_filename):
+
+    mindist = 5.  # ignore sources <= 0mm from inner skull
+    fwd = mne.make_forward_solution(raw_fpath, trans_fname, src, bem,
+                                mindist=mindist, meg=False, eeg=True,
+                                n_jobs=2)
+
+
+    fwd_filename = op.join(data_path, fwd_filename + '-fwd.fif')
+    mne.write_forward_solution(fwd_filename, fwd, overwrite=True)
